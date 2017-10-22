07de6b872832d92e2f7ba1d5ba4a6fb3aea278b7
NastyaPop <32039643+NastyaPop@users.noreply.github.com>
2017-10-22 19:54:19 +0300

    Update lab01.1.py

--- lab01.1.py
+++ lab01.1.py
@@ -1,13 +1,23 @@
-#import math
+import math
 from math import cosh, log
+
+a = int(input('Enter a: '))
+x = int(input('Enter x: '))
+
+G = 10 * (10 * a ** 2 + 11 * a * x - 25 * x ** 2)/(- a ** 2 + a * x + 6 * x ** 2)
+
+print('A={0}, X={1}, Result: {2:.6f}'.format(a, x, G))
+
+a = float(input('Enter a: '))
+x = float(input('Enter x: '))
+
+F = cosh(21 * a ** 2 - 34 * a * x + 9 * x ** 2)
+
+print('A={0}, X={1}, Result: {2:.6f}'.format(a, x, F))
 
 a = int (input('Enter a: '))
 x = int (input('Enter x: '))
 
-G = 10 * (10 * a ** 2 + 11 * a * x - 25 * x ** 2)/(- a ** 2 + a * x + 6 * x ** 2)
+Y = log(3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1)/log(10)
 
-F = cosh (21 * a ** 2 - 34 * a * x + 9 * x ** 2)
-
-Y = log (3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1)/log(10)
-
-print('G = {}, F = {}, Y = {}'.format(G, F, Y))
+print('A={0}, X={1}, Result: {2:.6f}'.format(a, x, Y))
